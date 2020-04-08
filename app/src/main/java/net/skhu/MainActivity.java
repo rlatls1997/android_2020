c758ff2889c73a067470e38f623dbbcc6bc3ece5
Kim, Sinhyuk <rlatls1997@gmail.com>
2020-04-08 22:01:54 +0900

    second commit

--- app/src/main/java/net/skhu/MainActivity.java
+++ app/src/main/java/net/skhu/MainActivity.java
@@ -36,6 +36,10 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, ButtonsActivity.class);
             startActivity(intent);
             return true;
+        } else if (id == R.id.action_checkboxes){
+            Intent intent = new Intent(this, CheckboxesActivity.class);
+            startActivity(intent);
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
